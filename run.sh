997af653b3d09621a8b311e27462a74c220066d9
michael <mdcatchen@gmail.com>
2023-09-29 13:50:20 -0400

    ok

--- run.sh
+++ run.sh
@@ -7,3 +7,13 @@
 #SBATCH --cpus-per-task=1        
 #SBATCH --mem-per-cpu=64G      
 #SBATCH --time=04:00:00         
+
+
+module load cuda
+module load julia/1.8.5
+module load cudnn 
+
+export JULIA_DEPOT_PATH="/project/def-gonzalez/mcatchen/JuliaEnvironments/COBees"
+export CLUSTER="true"
+
+julia main.jl
